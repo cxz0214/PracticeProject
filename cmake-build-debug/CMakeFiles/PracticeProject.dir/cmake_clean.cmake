7665f9ac9dfe22808cc3a8e71acf86d04d160afd
cxz0214 <40239400+cxz0214@users.noreply.github.com>
2020-12-14 14:25:43 +0800

    第一次

---
+++ cmake-build-debug/CMakeFiles/PracticeProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PracticeProject.dir/main.cpp.obj"
+  "PracticeProject.exe"
+  "PracticeProject.exe.manifest"
+  "PracticeProject.pdb"
+  "libPracticeProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PracticeProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
